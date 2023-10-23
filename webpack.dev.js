6c94bd7d087d992a7ad7c06ec71000d17e8f0c44
Ivan Vedenin <steven.marelly@gmail.com>
2023-10-23 20:16:20 +0300

    change folder name

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
     devtool: 'eval-source-map',
     devServer: {
         static: {
-            directory: path.join(__dirname, 'dist'),
+            directory: path.join(__dirname, 'docs'),
         },
         hot: true,
     },
